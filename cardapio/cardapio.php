cdfb4f3e09f289581a9e83cea6b03817947ac626
PauloALeonel <150287169+PauloALeonel@users.noreply.github.com>
2024-11-24 16:25:22 -0300

    Add files via upload

---
+++ cardapio/cardapio.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hambúrguer | BRUTUS</title>
+    <link rel="icon" href="..\img\favicon.svg" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="custom.css"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="custom.css"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../custom.css">
+    <style>
+        .comp{
+            background-color:saddlebrown;
+        }
+    </style>
+ 
+</head>
+<body>
+    <?php 
+        include_once "../cabecalho.html";
+        $host = "localhost";
+        $database = "brutus";
+        $username = "root";
+        $password = ""; 
+     
+     
+        $conn = new PDO("mysql:host=$host;dbname=" . $database, $username, $password);
+
+        $query_products = "SELECT cod_item, nome, descricao, preco, imagem FROM itens";
+        $result_products = $conn->prepare($query_products);
+        $result_products->execute();
+    ?>
+    <div class="container my-5">
+        <h2 class="text-center mb-4 logo">BURGUER</h2>
+		<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3">
+            <?php 
+            $contador = 0;
+            while ($row_product = $result_products->fetch()) {
+                extract($row_product); 
+                if($contador>=4){$contador=1;}
+                else{
+                    $contador=$contador+1;
+                }
+            ?>
+                <!--produto-->
+                <div class="col">
+                    <div class="card">
+                    <?php   echo "<img src='../produtos/$imagem' class='card-img-top' alt='Produto $contador'><br>";
+                            echo "<div class='card-body'>";
+                            echo"<h5 class='card-title'>$nome</h5>";
+                            echo"<p class='card-preco'>R$ $preco</p>";
+                            echo"<p class='card-text'>$descricao</p>"; ?>
+                            <button class="comp btn w-100 comprar">Comprar</button>
+                    </div>
+                    </div>
+                </div>
+            <?php }?>
+        </div>
+    </div>
+    <?php include_once "../rodape.html"; ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
