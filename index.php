b547019eacc5bbf7e8ad56927979f95a5fbb47e6
PauloALeonel <150287169+PauloALeonel@users.noreply.github.com>
2024-12-04 01:33:10 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php 
+$host = "localhost"; 
+$database = "brutus"; 
+$username = "root"; 
+$password = ""; 
+$conn = new PDO("mysql:host=$host;dbname=" . $database, $username, $password); 
+
+$query_products = "SELECT cod_item, nome, descricao, preco, imagem FROM itens WHERE fk_Categoria_cod_categoria = 2"; 
+$result_products = $conn->prepare($query_products); 
+$result_products->execute(); 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,9 +40,9 @@
             <div class="carousel-item">
                 <img src="img\bannerkids.svg" class="d-block w-100" alt="...">
             </div>
-            <div class="carousel-item">
+            <!--<div class="carousel-item">
                 <img src="img\banner3.svg" class="d-block w-100" alt="...">
-            </div>
+            </div> -->
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -41,7 +53,34 @@
             <span class="visually-hidden">Próximo</span>
         </button>
     </div> 
-    
+    <div class="container my-5">
+    <h2 class = "logo">combos</h2>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3">
+        <?php 
+        $contador = 0; 
+        while ($row_product = $result_products->fetch()) { 
+            extract($row_product); 
+            if ($contador >= 4) { 
+                $contador = 1; 
+            } else { 
+                $contador++; 
+            } 
+        ?>
+        <div class="col">
+            <div class="card">
+                <img src='produtos/<?php echo $imagem; ?>' class='card-img-top' alt='Produto <?php echo $contador; ?>'>
+                <div class='card-body'>
+                    <h5 class='card-title'><?php echo $nome; ?></h5>
+                    <p class='card-preco'>R$ <?php echo $preco; ?></p>
+                    <p class='card-text'><?php echo $descricao; ?></p>
+                    <button class='comp btn w-100 comprar'>Comprar</button>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+</div>
+
       <!--#include file='rodape.html'-->
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
